110dd81d30d1b0a605c360739f41fa78e1db668e
Juhyorim <joohr1234@gmail.com>
2023-07-05 21:49:11 +0900

    initial commit

---
+++ src/main/java/dev/lime/subtest/SubTestApplication.java
@@ -0,0 +1,15 @@
+package dev.lime.subtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubTestApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/dev/lime/subtest/SubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lime.subtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
